17fb6cf14dc3f10aab08beb8b5f55bd4a9d270eb
Alex808r <fromxlam@gmail.com>
2022-01-14 09:25:51 +0300

    DB schema & seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_13_151305) do
+ActiveRecord::Schema.define(version: 2022_01_13_203641) do
+
+  create_table "event_categories", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "race_id", null: false
+    t.integer "event_category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_category_id"], name: "index_events_on_event_category_id"
+    t.index ["race_id"], name: "index_events_on_race_id"
+  end
 
   create_table "races", force: :cascade do |t|
     t.string "name", null: false
@@ -20,4 +36,6 @@ ActiveRecord::Schema.define(version: 2022_01_13_151305) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "events", "event_categories"
+  add_foreign_key "events", "races"
 end
